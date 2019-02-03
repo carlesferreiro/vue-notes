44db2d8a4611d80e3b992506c2ec1d84b5db3fae
Carles Ferreiro Casanovas <carles.ferreiro@gmail.com>
2019-02-03 17:00:14 +0100

    add and remove notes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+
+		<div class="container">
+			<h1 class="has-text-centered is-size-3">Notes importants</h1>
+			<Notes :notes="notes" @remove="deleteNote" />
+		</div>
+
+		<nav class="navbar is-fixed-bottom">
+			<div class="form container">
+				<div class="columns full">
+					<div class="column is-10"><input class="input" v-model="draft" @keyup.enter="addNote" /></div>
+					<div class="column is-2"><button class="button full" @click="addNote">Afegir</button></div>
+				</div>
+			</div>
+		</nav>
+
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import Notes from './components/Notes.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			Notes
+		},
+		data() {
+			return {
+				draft: '',
+				notes: []
+			}
+		},
+		methods: {
+			addNote() {
+				let id = this.notes.length+1;
+				this.notes.push({
+					id,
+					draft: this.draft,
+				});
+				this.draft = '';
+			},
+			deleteNote(id) {
+				let index = this.notes.findIndex(note => note.id == id);
+				if(index>=0) this.notes.splice(index, 1);
+			}
+		}
+	}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+	height: 100%;
+}
+h1 {
+	margin: 1em 0;
+}
+.full {
+	width: 100%;
+}
+nav.navbar {
+	padding-top: 1em;
+	background-color: #eee;
 }
 </style>
---
+++ src/components/Notes.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="note in notes">
+        <button @click="remove(note.id)">&times;</button>
+        {{ note.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Notes',
+    props: {
+      notes: Array
+    },
+    methods: {
+     remove(id) {
+      this.$emit('remove', id);
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+
+  li {
+    position: relative;
+    display: inline-block;
+    width: 200px;
+    padding: 1em 1.5em;
+    margin: 0.5em;
+    color: #333;
+    background: #fff684;
+    overflow: hidden;
+    border-left: 1px solid #ddd;
+    border-bottom: 1px solid #ddd;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      border-width: 0 16px 16px 0;
+      border-style: solid;
+      border-color: #fff #fff #d6cd2f #d6cd2f;
+      background: #d6cd2f;
+      box-shadow: 0 1px 1px rgba(0,0,0,0.1), -1px 1px 1px rgba(0,0,0,0.1);
+    }
+
+    button {
+      position: absolute;
+      right: 10px;
+      bottom: 10px;
+      background: transparent;
+      border: 0px;
+      font-weight: bold;
+      font-size: 20px;
+      line-height: 1em;
+      padding: 0px;
+      margin: 0px;
+      cursor: pointer;
+    }
+  }
+}
+</style>
